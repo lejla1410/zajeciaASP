e81cc8418ba532516684ba24258d73ef68183539
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-11-18 15:11:22 +0100

    Początki formularzu kontaktowego

---
+++ FormularzKontaktowy/FormularzKontaktowy/Models/FkDBList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FormularzKontaktowy.Models
+{
+    public class FkDBList
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+
+    }
+}
---
+++ FormularzKontaktowy/FormularzKontaktowy/Models/FormContext.cs
@@ -0,0 +1,21 @@
+﻿using FormularzKontaktowy.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace FormularzKontaktowy.Models
+{
+    public class FormContext : DbContext
+    {
+
+        public FormContext() : base("FormularzK")
+        {
+
+        }
+        public DbSet<FkDBList> LzDBTest { get; set; }
+
+
+    }
+}
