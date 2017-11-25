4827e75a118212283e66bf3c805c9405719dcebb
Kasia Nagel <katarzyna.nagel@wp.pl>
2017-11-25 10:59:41 +0100

    haslo do 8 znaków

---
+++ ProjektTest/ProjektTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ProjektTest.MvcApplication" Language="C#" %>
---
+++ ProjektTest/ProjektTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ProjektTest.Startup))]
+namespace ProjektTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
